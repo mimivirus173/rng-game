286d8815815f927c47c417e8353bd830446e6bc2
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-06-12 12:47:30 +0100

    basic program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace pointGame
 {
@@ -6,7 +7,39 @@ namespace pointGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random numGen = new Random();
+            
+            // Variables
+            int points = 0;
+            int number;
+
+            // Game
+            for (int i = 0; i == 0; i++)
+            {
+                Console.Write("Write a number between 1 and 99: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number > 99) {
+                Console.WriteLine("Invalid number!");
+                return;
+            }
+            Console.WriteLine("You chose: " + number);
+            int roll = numGen.Next(1,100);
+            Console.WriteLine("Computer chose: " + roll);
+
+            if (roll == number) {
+                Console.WriteLine("Computer picked the same number as you!");
+                Console.WriteLine("You lose " + roll + " points.");
+                points -= roll;
+                return;
+            } else {
+            Console.WriteLine("You gain " + number + " points.");
+            points += number;
+            Console.WriteLine("Points: " + points);
+            }
+            }
+        
+            // Wait before closing
+            Console.ReadKey();
         }
     }
 }
