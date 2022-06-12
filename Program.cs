eaf4b6d6dd9dfc475ec3713ec3b6840e955c0741
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-06-12 17:15:25 +0100

    added much stuff
    colours, formatting, more text

--- Program.cs
+++ Program.cs
@@ -32,9 +32,8 @@ namespace rngGame
             while (true)
             {
                 // Prompt the user
-                Console.Write("Write a number between 1 and 99: ");
+                Console.Write("Write a number between 1 and 99: ", Console.ForegroundColor = ConsoleColor.White);
                 string input = Console.ReadLine();
-                number = Convert.ToInt32(input);
 
                 // Check if inputted number is valid
                 if (int.TryParse(input, out number)) {
@@ -42,26 +41,28 @@ namespace rngGame
                         int roll = numGen.Next(1, 100);
 
                         if (roll > number) {
-                            Console.WriteLine("You win!");
+                            Console.WriteLine("You win!", Console.ForegroundColor = ConsoleColor.Green);
 
                             // Gain points
-                            Console.WriteLine("You gain " + number + " points.");
+                            Console.WriteLine("You gain " + number + " points.", Console.ForegroundColor = ConsoleColor.White);
                             points += number;
-
+                            
                             Console.WriteLine("Current points: " + points);
+                            Console.WriteLine("-------------------------");
                         } else {
                             // Loss
-                            Console.WriteLine("You lose!");
-                            points = 0;
-
                             Console.Clear();
+
+                            Console.WriteLine("You lose!", Console.ForegroundColor = ConsoleColor.DarkRed);
+                            Console.WriteLine("-------------------------", Console.ForegroundColor = ConsoleColor.White);
+                            points = 0;
                             break;
                         }                    
                     } else {
-                        Console.WriteLine("Invalid input!");
+                        Console.WriteLine("Invalid input!", Console.ForegroundColor = ConsoleColor.Red);
                     };
                 } else {
-                    Console.WriteLine("Invalid input!");
+                    Console.WriteLine("Invalid input!", Console.ForegroundColor = ConsoleColor.Red);
                 }
             }
         }
